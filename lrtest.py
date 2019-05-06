291ea8ad8f1112eaa6b0be2b978522a6aa0a60ee
Nathasha <Nathasha@Warunis-MacBook-Air.local>
2019-05-06 20:05:45 +1000

    Added test flying script

---
+++ lrtest.py
@@ -0,0 +1,27 @@
+from djitellopy import Tello
+import cv2
+import time
+
+tello = Tello()
+
+tello.connect()
+
+tello.takeoff()
+time.sleep(5)
+
+tello.move_left(100)
+time.sleep(5)
+
+tello.move_right(100)
+time.sleep(5)
+
+tello.rotate_counter_clockwise(45)
+time.sleep(5)
+
+tello.rotate_clockwise(45)
+time.sleep(5)
+
+tello.land()
+time.sleep(5)
+
+tello.end()
